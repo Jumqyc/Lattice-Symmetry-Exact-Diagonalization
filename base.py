b6c6ed026747f35cff170b4c8b945141b9a8ecea
jumqyc <jumqyc@gmail.com>
2026-07-30 21:07:22 +0200

    added multiple orbit support

--- base.py
+++ base.py
@@ -9,15 +9,14 @@ class Lattice:
     '''
     def __init__(self, 
                  supercell_info:list[tuple[np.ndarray,int]],
-                 cell_info: dict[str, np.ndarray], 
+                 cell_info: dict[tuple[str,int], np.ndarray], 
                  MAX_EXTENSION = 1
                  ):
         '''
-        Initialize a Lattice object.
-        A lattice (supercell) is defined by its reciprocal vectors and their multiplicities, as well as additional cell information.
+        Initialize a Lattice object. A lattice (supercell) is defined by its reciprocal vectors and their multiplicities, as well as additional cell information.
         Arguments:
-        - supercell_info: A variable number of tuples, each containing a supercell vector (as a numpy array) and its multiplicity (as an integer).
-        - cell_info: Additional information about the cell, provided as keyword arguments. Each key is a string describing the atom, and each value is a numpy array representing the atom's position in the cell.
+        - supercell_info: A variable number of tuples, each containing a supercell vector (as a numpy array) and its multiplicity (as an integer). Eventually, the supercell vectors will be multiplied by their respective multiplicities to form the complete supercell.
+        - cell_info: Additional information about the cell, provided as keyword arguments. Each key is a tuple of (atom_type, atom_index), and each value is a numpy array representing the atom's position in the cell.
         - tol: Tolerance for numerical comparisons. 
         '''
         self.spacial_dim = len(supercell_info[0])
@@ -28,11 +27,12 @@ class Lattice:
         self.supercell_vectors =  np.array([vec*m for vec, m in supercell_info])
         self.reciprocal_vectors = np.array([vec for vec, _ in supercell_info])
 
-        self.unit_cell_idx: list[tuple[np.ndarray, int]] = []
+        self.unit_cell_idx: list[tuple[np.ndarray,str, int]] = []
         for n in product(*[range(m) for _, m in supercell_info]):
-            for vec in cell_info.values():
+            for (s,_),vec in cell_info.items():
                 self.unit_cell_idx.append(
                     (vec + np.array(n) @ self.reciprocal_vectors ,
+                    s,
                     len(self.unit_cell_idx))) 
                     # centering the cell at the origin and assigning a unique index to each atom
         self.size_of_unit_cell = len(self.unit_cell_idx)
@@ -40,14 +40,15 @@ class Lattice:
 
         # extending by PBC:
         self.extended_cell = []
-        for pos, idx in self.unit_cell_idx: # to avoid modifying the list while iterating over it
+        for pos, s, idx in self.unit_cell_idx: # to avoid modifying the list while iterating over it
             for n in product(
                         range(-MAX_EXTENSION, MAX_EXTENSION + 1),
                         repeat=self.spacial_dim):
                 self.extended_cell.append(
-                    (pos + np.array(n) @ self.supercell_vectors, idx)
+                    (pos + np.array(n) @ self.supercell_vectors, s, idx)
                 )
-    def center_at(self, center):
+
+    def set_center_at(self, center):
         '''
         Center the lattice at a specific atom index or position.
         Arguments:
@@ -56,11 +57,11 @@ class Lattice:
         '''
         if isinstance(center, int):
             assert 0 <= center < self.size_of_unit_cell, f"Center index must be between 0 and {self.size_of_unit_cell-1}."
-            center_pos,_ = self.unit_cell_idx[center]
+            center_pos, _, _ = self.unit_cell_idx[center]
         else:
             center_pos = np.asarray(center)
-        self.extended_cell = [(pos - center_pos, idx) for pos, idx in self.extended_cell]
-        self.unit_cell_idx = [(pos - center_pos, idx) for pos, idx in self.unit_cell_idx]
+        self.extended_cell = [(pos - center_pos, s, idx) for pos, s, idx in self.extended_cell]
+        self.unit_cell_idx = [(pos - center_pos, s, idx) for pos, s, idx in self.unit_cell_idx]
 
     def plot(self, ax=None):
         '''
@@ -71,11 +72,11 @@ class Lattice:
         if ax is None:
             fig, ax = plt.subplots(figsize=(6, 6))
         
-        for pos, idx in self.extended_cell:
+        for pos, s, idx in self.extended_cell:
             ax.scatter(*pos, alpha=0.6)
             ax.text(*pos, str(idx), fontsize=8, ha='center', va='center')
 
-        for pos, idx in self.unit_cell_idx:
+        for pos, s, idx in self.unit_cell_idx:
             ax.scatter(*pos, color='red', alpha=0.8)
             ax.text(*pos, str(idx), fontsize=10, ha='center', va='center', color='white')
         ax.set_aspect('equal')
@@ -87,9 +88,9 @@ class Lattice:
         return ax
 
 
-    def _get_idx(self, pos,tol = 1e-6) -> int:
-        for p,idx in self.extended_cell:
-            if np.allclose(p, pos, atol=tol):
+    def _get_idx(self, pos,s,tol = 1e-6) -> int:
+        for p,s1,idx in self.extended_cell:
+            if np.allclose(p, pos, atol=tol) and s1 == s:
                 return idx
         raise ValueError(f"Position {pos} not found in unit cell nor extended cell.")
 
@@ -102,9 +103,9 @@ class Lattice:
 
         permuted = np.zeros(self.size_of_unit_cell, dtype=int)
 
-        for pos, idx in self.unit_cell_idx:
+        for pos, s, idx in self.unit_cell_idx:
             transformed_pos = arr @ pos
-            permuted[idx] = self._get_idx(transformed_pos)
+            permuted[idx] = self._get_idx(transformed_pos, s)
         if np.array_equal(np.sort(permuted), np.arange(self.size_of_unit_cell)) == False:
             raise ValueError(f"{arr} is not a valid permutation, as permuted = {np.sort(permuted)}.")
         
@@ -116,8 +117,8 @@ class Lattice:
         assert 0 <= dir < self.spacial_dim, f"Translation direction must be between 0 and {self.spacial_dim-1}."
 
         translated = np.zeros(self.size_of_unit_cell, dtype=int)
-        for pos,idx in self.unit_cell_idx:
-            translated[idx] = self._get_idx(pos + self.reciprocal_vectors[dir])
+        for pos, s, idx in self.unit_cell_idx:
+            translated[idx] = self._get_idx(pos + self.reciprocal_vectors[dir], s)
         return translated
 
     
